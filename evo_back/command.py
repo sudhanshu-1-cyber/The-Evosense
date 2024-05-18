99b120889ab48474fe143d2b63c53fbf9fe38fcd
sudhanshu-1-cyber <sudhanshupandey180.hld@gmail.com>
2024-05-18 19:54:36 +0530

    speech recognition and evosense voice

---
+++ evo_back/command.py
@@ -0,0 +1,29 @@
+import pyttsx3
+import speech_recognition as sr
+
+def speak(text):
+    engine=pyttsx3.init('sapi5')
+    voices=engine.getProperty('voices')
+    engine.setProperty('voice', voices[0].id)
+    engine.setProperty('rate', 175)
+    engine.say(text)
+    engine.runAndWait()
+
+def takecommand():
+    r=sr.Recognizer()
+    with sr.Microphone() as source:
+        print("listening...")
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source)
+        audio = r.listen(source,  10,  6)
+    try:
+        print('recognizing...')
+        query= r.recognize_google(audio, language='en-in')
+        print(f"{query}")
+    except Exception as e:
+        return ""
+    return query.lower()
+
+text = takecommand()
+
+speak(text)
