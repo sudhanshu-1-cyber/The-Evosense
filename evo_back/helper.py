7402d896258964cc3062a0354fb329488e336a6e
sudhanshu-1-cyber <sudhanshupandey180.hld@gmail.com>
2024-06-01 07:50:31 +0530

    done

--- evo_back/helper.py
+++ evo_back/helper.py
@@ -2,7 +2,7 @@ import re
 
 def extract_yt_term(command):
     # Define a regular expression pattern to capture request for youtube
-    pattern = r'play\s+(.*?)\s+on\s+youtube'
+    pattern = r'play\s+(.*?)\(?:on|from)\s+youtube'
     #Use re.search to find the match in the command
     match = re.search(pattern, command, re.IGNORECASE)
     #if match is found , return the extracted request; otherwise, return none
